8b88aaede43c38bf8a638bdcf950d7bc576680d2
Heber Ribeiro <heber.r@outlook.com>
2020-05-23 19:09:25 -0300

    Domain layer

---
+++ src/main/java/me/salvus/patients/domain/Patient.java
@@ -0,0 +1,102 @@
+package me.salvus.patients.domain;
+
+import java.io.Serializable;
+
+public class Patient implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String phone;
+	private String address;
+	private String city;
+	private String state;
+
+	public Patient() {
+
+	}
+
+	public Patient(Integer id, String name, String phone, String address, String city, String state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
