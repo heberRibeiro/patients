0c4121d45f71f7690accfddafdeab09c2b4eaf95
Heber Ribeiro <heber.r@outlook.com>
2020-05-23 19:08:28 -0300

    Project started

---
+++ src/test/java/me/salvus/patients/PatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.salvus.patients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
