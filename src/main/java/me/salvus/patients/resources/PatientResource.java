16382423c474845bf1d800cfdf6e62baf7539261
Heber Ribeiro <heber.r@outlook.com>
2020-05-24 18:32:49 -0300

    Finished customization the api with swagger

--- src/main/java/me/salvus/patients/resources/PatientResource.java
+++ src/main/java/me/salvus/patients/resources/PatientResource.java
@@ -20,9 +20,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import me.salvus.patients.domain.Patient;
 import me.salvus.patients.services.PatientService;
 
+// Swagger customization @Api(), @ApiOperation and @ApiOperation
+@Api(value = "Patients Endpoint", description = "Application endpoints", tags = { "Patients Endpoint" })
 @RestController
 @RequestMapping("/api")
 public class PatientResource {
@@ -30,11 +35,13 @@ public class PatientResource {
 	@Autowired
 	private PatientService service;
 
+	@ApiOperation(value = "Finds all patients passing the appropriate parameters")
 	@GetMapping(value = "/patients")
-	public ResponseEntity<List<Patient>> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
-			@RequestParam(value = "limit", defaultValue = "5") int limit,
-			@RequestParam(value = "attribute", defaultValue = "id") String attribute,
-			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+	public ResponseEntity<List<Patient>> findAll(
+			@ApiParam(value = "Indicates the page number fetched starting at zero") @RequestParam(value = "page", defaultValue = "0") int page,
+			@ApiParam(value = "Indicates the number of patients searched per page") @RequestParam(value = "limit", defaultValue = "5") int limit,
+			@ApiParam(value = "Indicates the attribute that will be used to order the search") @RequestParam(value = "attribute", defaultValue = "id") String attribute,
+			@ApiParam(value = "Defines whether the ordering will be increasing or decreasing") @RequestParam(value = "direction", defaultValue = "asc") String direction) {
 
 		Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 
@@ -45,16 +52,19 @@ public class PatientResource {
 
 	}
 
+	@ApiOperation(value = "Finds the patient according to the id")
 	@GetMapping(value = "/patients/{id}")
-	public ResponseEntity<Patient> findById(@PathVariable Integer id) {
+	public ResponseEntity<Patient> findById(@ApiParam(value = "Patient id for search") @PathVariable Integer id) {
 
 		Patient patient = service.findById(id);
 		return ResponseEntity.ok().body(patient);
 
 	}
 
+	@ApiOperation(value = "Inserts patient passing json through the request body")
 	@PostMapping(value = "/patients")
-	public ResponseEntity<Void> insert(@RequestBody Patient patient) {
+	public ResponseEntity<Void> insert(
+			@ApiParam(value = "Json that will come in the body of the request with information of the patient to be inserted") @RequestBody Patient patient) {
 
 		patient.setId(null);
 		Patient obj = service.insert(patient);
@@ -64,15 +74,19 @@ public class PatientResource {
 
 	}
 
+	@ApiOperation(value = "Update the patient according to the id and json through the request body")
 	@PutMapping(value = "/patients/{id}")
-	public ResponseEntity<Patient> update(@RequestBody Patient patient, @PathVariable Integer id) {
+	public ResponseEntity<Patient> update(
+			@ApiParam(value = "Json that will come in the body of the request with information of the patient to be updated") @RequestBody Patient patient,
+			@ApiParam(value = "Patient id to be changed") @PathVariable Integer id) {
 
 		Patient obj = service.update(patient, id);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@ApiOperation(value = "Delete the patient according to the id") // Swagger customization
 	@DeleteMapping(value = "patients/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+	public ResponseEntity<Void> delete(@ApiParam(value = "Patient id to be deleted") @PathVariable Integer id) {
 
 		service.delete(id);
 		return ResponseEntity.noContent().build();
