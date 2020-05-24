20ba587d1009da2967cd25b05e03b22c175474fb
Heber Ribeiro <heber.r@outlook.com>
2020-05-24 11:00:37 -0300

    Error Handling

--- src/main/java/me/salvus/patients/services/PatientService.java
+++ src/main/java/me/salvus/patients/services/PatientService.java
@@ -3,27 +3,34 @@ package me.salvus.patients.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import me.salvus.patients.domain.Patient;
 import me.salvus.patients.repositories.PatientRepository;
+import me.salvus.patients.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PatientService {
-	
+
 	@Autowired
 	private PatientRepository repository;
-	
+
 	public List<Patient> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Patient insert(Patient patient) {
 		return repository.save(patient);
 	}
-	
+
 	public void delete(Integer id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException("Patient not found in data base.");
+		}
 	}
 
 }
