614df6a967923f3e1896028f25882676524f95b6
Heber Ribeiro <heber.r@outlook.com>
2020-05-24 13:15:32 -0300

    Resource and service layer - update patient

--- src/main/java/me/salvus/patients/resources/PatientResource.java
+++ src/main/java/me/salvus/patients/resources/PatientResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +50,13 @@ public class PatientResource {
 
 		return ResponseEntity.created(uri).build();
 
+	}
+	
+	@PutMapping(value = "/patients/{id}")
+	public ResponseEntity<Patient> update(@RequestBody Patient patient, @PathVariable Integer id) {
+		
+		Patient obj = service.update(patient, id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 	@DeleteMapping(value = "patients/{id}")
--- src/main/java/me/salvus/patients/services/PatientService.java
+++ src/main/java/me/salvus/patients/services/PatientService.java
@@ -3,6 +3,8 @@ package me.salvus.patients.services;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -21,9 +23,8 @@ public class PatientService {
 		return repository.findAll();
 	}
 
-	
 	public Patient findById(Integer id) {
-		
+
 		try {
 			Patient patient = repository.findById(id).get();
 			return patient;
@@ -35,6 +36,24 @@ public class PatientService {
 
 	public Patient insert(Patient patient) {
 		return repository.save(patient);
+	}
+
+	public Patient update(Patient patient, Integer id) {
+
+		try {
+			Patient obj = repository.getOne(id); // return a reference to the entity
+			obj.setName(patient.getName());
+			obj.setPhone(patient.getPhone());
+			obj.setAddress(patient.getAddress());
+			obj.setCity(patient.getCity());
+			obj.setState(patient.getState());
+
+			return repository.save(obj);
+
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Patient not found in data base to be updated.");
+		}
+
 	}
 
 	public void delete(Integer id) {
