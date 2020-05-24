6a2d0c0d1e92578f7d2fc27c54a7f7a00d7d7c61
Heber Ribeiro <heber.r@outlook.com>
2020-05-24 14:10:35 -0300

    Support flyway

--- src/main/java/me/salvus/patients/PatientsApplication.java
+++ src/main/java/me/salvus/patients/PatientsApplication.java
@@ -1,35 +1,15 @@
 package me.salvus.patients;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import me.salvus.patients.domain.Patient;
-import me.salvus.patients.repositories.PatientRepository;
-
 @SpringBootApplication
-public class PatientsApplication implements CommandLineRunner {
+public class PatientsApplication {
 	
-	@Autowired
-	private PatientRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientsApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Patient pac01 = new Patient(null, "José", "(81)9999999", "address", "Recife", "PE");
-		Patient pac02 = new Patient(null, "Maria", "(81)9999999", "address", "Recife", "PE");
-		Patient pac03 = new Patient(null, "Benedito", "(81)9999999", "address", "Recife", "PE");
-		Patient pac04 = new Patient(null, "Ana", "(81)9999999", "address", "Recife", "PE");
-		
-		repo.saveAll(Arrays.asList(pac01, pac02, pac03, pac04));
-		
-	}
-
+	
 }
